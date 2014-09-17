d52cb858aa649796cc3b4db70c82ac44fa60d364
khdesignverignde <kh@designverign.de>
2014-09-17 14:06:03 +0200

    v6

--- www/js/index.js
+++ www/js/index.js
@@ -28,6 +28,11 @@ var app = {
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
         document.getElementById('scan').addEventListener('click', this.scan, false);
+        document.getElementById('camera').addEventListener('click', this.camera, false);
+        document.getElementById('device').addEventListener('click', this.device, false);
+        document.getElementById('network-information').addEventListener('click', this.networkInformation, false);
+        $('#input .save').on('click', this.input.save);
+        $('#input .load').on('click', this.input.load);
     },
     // deviceready Event Handler
     //
@@ -47,7 +52,73 @@ var app = {
 
         console.log('Received Event: ' + id);
     },
-	scan: function() {
+    output: {
+        _el: $('#output'),
+        objectProperties: function(obj, name){
+            if(!name) name = 'object';
+            
+            var str = '<br><br>' + name + ':<br>---------------------------------------------------------<br>';
+            
+            for(var i in obj){
+                str += i + ': ' + obj[i] + '<br>';
+            }
+            
+            this._el.html(str + this._el.html());
+        },
+        string: function(str){
+            str += '<br><br>string:<br>---------------------------------------------------------<br>';
+            this._el.html(str + this._el.html());
+        }
+    },
+    storage: function(name, method, obj){
+        if(!method) method = 'save';
+        
+        var storage = window.localStorage;
+        
+        switch(method){
+            case 'show':
+                app.output
+                break;
+            case 'remove':
+                storage.removeItem(name);
+                return true;
+                break;
+            case 'load':
+                var item = storage.getItem(name);
+                
+                if(item){
+                    return item;
+                } else {
+                    return false;
+                }
+                break;
+            default:
+            case 'save':
+//                var exists = false;
+//                if(storage.getItem(name)) exists = true;
+                
+                storage.setItem(name, obj);
+        }
+        
+    },
+    input:{
+        _el: $('#input textarea'),
+        load: function(){
+            
+        },
+        save: function(){
+            var val = this._el.val();
+            
+            app.storage('input', 'save', val);
+        },
+        load: function(){
+            var val = app.storage('input', 'load');
+            
+            app.output.string(val);
+            
+        }
+    },
+    scan: function() {
         console.log('scanning');
         
         var scanner = cordova.require("cordova/plugin/BarcodeScanner");
@@ -74,5 +145,29 @@ var app = {
         }, function (error) { 
             console.log("Scanning failed: ", error); 
         } );
+    },
+    camera: function(){
+        navigator.camera.getPicture(
+            //success
+            function (imageData) {
+                var $image = $('#photo');
+                $image.attr('src', 'data:image/jpeg;base64,' + imageData);
+            }, 
+            //fail
+            function (message) {
+                alert('Failed because: ' + message);
+            },
+            //options
+            { 
+                quality: 50,
+                destinationType: Camera.DestinationType.DATA_URL 
+            }
+        );
+    },
+    device: function(){
+        app.output.objectProperties(device);
+    },
+    networkInformation: function(){
+        app.output.objectProperties(navigator.connection);
     }
 };
